dc0f90a85d62c895340a63eefea52a5ce4cb3bab
protossking <upgrademarine5@gmail.com>
2021-07-12 19:59:44 +0900

    Initial commit

---
+++ src/main/java/com/yang/mvcpratice/MvcpraticeApplication.java
@@ -0,0 +1,13 @@
+package com.yang.mvcpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcpraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcpraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yang/mvcpratice/MvcpraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.mvcpratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcpraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
